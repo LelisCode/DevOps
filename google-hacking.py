1b24005d0bce571b2f59fff65c9c9f149d1ed523
Mileni Lelis Ramos Servulo <lelis.engineer@yahoo.com>
2026-03-30 08:42:15 -0300

    Update google-hacking.py

--- google-hacking.py
+++ google-hacking.py
@@ -1,30 +1,33 @@
 import webbrowser
 
-opçoes=input("site, intitle, inurl, ext, link ou inanchor, eles contem boas combinações")
+opçoes=input("site, intitle, inurl, ext, link ou inanchor, alguns contem boas combinações para uma busca completa")
 escolha=input("Qual google hacking que usar hoje?")
 
 
 if escolha == "site":
     domino = input("Digite o domíno:")
-    palavra =input("Digite a palavra (ex:login):")
-
-    dork = f"site:{domino} intitle:{palavra}"
+    palavra =input("Digite um título pra página (ex:login):")
+    text    =input("Digite um texto que esteja dentro do site")
+    dork = f"site:{domino} | intitle:{palavra} | intext:{text}"
 
 elif escolha == "intitle":
     t=input("Digite o título")
     durl =input("Digite termos dentro da url")
-
-    dork = f"intitle:{t} inurl:{durl}"
+    text1    =input("Digite um texto que esteja dentro do site")
+    dork = f"intitle:{t} intext:{text1} | inurl:{durl}"
  
 elif escolha == "inurl":
-    i =input("Digite o termo a ser encontrado na URL")
-    dork = f"inurl:{i}"
+    i =input(" Digite o termo a ser encontrado na URL")
+    i2 =input(" Digite o site 1")
+    i3 =input(" Digite o site 2")
+   
+    dork = f"inurl:{i} | site:{i2} | site:{i3}"
 
 elif escolha == "ext":
     
     dominio = input("Digite o domínio:")                                                                 
     e= input("Digite o arquivo a se localizar,exemplo:pdf,.py,.txt") 
-    dork = f"site:{dominio} ext:{e}"
+    dork = f"site:{dominio} | ext:{e}"
     
 
 
